b40c93f4d9ef39db591536770ade3d97cd6a1e6c
FFTEAM-Thomas <fam.trichter@googlemail.com>
2015-03-20 00:59:50 +0100

    opkgmanager angepasst

--- src/gui/opkg_manager.cpp
+++ src/gui/opkg_manager.cpp
@@ -70,14 +70,14 @@ enum
 
 static const std::string pkg_types[OM_MAX] =
 {
-	OPKG_CL " list",
-	OPKG_CL " list-installed",
-	OPKG_CL " list-upgradable",
-	OPKG_CL " update",
-	OPKG_CL " upgrade ",
-	OPKG_CL " remove ",
-	OPKG_CL " info ",
-	OPKG_CL " install "
+	"opkg-cl list",
+	"opkg-cl list-installed",
+	"opkg-cl list-upgradable",
+	"opkg-cl update",
+	"opkg-cl upgrade ",
+	"opkg-cl remove ",
+	"opkg-cl info ",
+	"opkg-cl install "
 };
 
 COPKGManager::COPKGManager()
@@ -103,8 +103,7 @@ int COPKGManager::exec(CMenuTarget* parent, const std::string &actionKey)
 		return showMenu();
 	}
 	int selected = menu->getSelected() - menu_offset;
-
-	if (expert_mode && actionKey == "rc_blue") {
+	if (expert_mode && actionKey == "rc_spkr") {
 		if (selected < 0 || selected >= (int) pkg_vec.size() || !pkg_vec[selected]->installed)
 			return menu_return::RETURN_NONE;
 
@@ -126,7 +125,7 @@ int COPKGManager::exec(CMenuTarget* parent, const std::string &actionKey)
 		execCmd(pkg_types[OM_INFO] + pkg_vec[selected]->name, true, true);
 		return res;
 	}
-	if (actionKey == "rc_red") {
+	if (actionKey == "rc_setup") {
 		expert_mode = !expert_mode;
 		updateMenu();
 		return res;
@@ -175,16 +174,16 @@ int COPKGManager::exec(CMenuTarget* parent, const std::string &actionKey)
 
 #define COPKGManagerFooterButtonCount 3
 static const struct button_label COPKGManagerFooterButtons[COPKGManagerFooterButtonCount] = {
-	{ NEUTRINO_ICON_BUTTON_RED, LOCALE_OPKG_BUTTON_EXPERT_ON },
+	{ NEUTRINO_ICON_BUTTON_BLUE, LOCALE_OPKG_BUTTON_EXPERT_ON },
 	{ NEUTRINO_ICON_BUTTON_INFO_SMALL, LOCALE_OPKG_BUTTON_INFO },
 	{ NEUTRINO_ICON_BUTTON_OKAY,	   LOCALE_OPKG_BUTTON_INSTALL }
 };
 #define COPKGManagerFooterButtonCountExpert 4
 static const struct button_label COPKGManagerFooterButtonsExpert[COPKGManagerFooterButtonCountExpert] = {
-	{ NEUTRINO_ICON_BUTTON_RED, LOCALE_OPKG_BUTTON_EXPERT_OFF },
+	{ NEUTRINO_ICON_BUTTON_BLUE, LOCALE_OPKG_BUTTON_EXPERT_OFF },
 	{ NEUTRINO_ICON_BUTTON_INFO_SMALL, LOCALE_OPKG_BUTTON_INFO },
 	{ NEUTRINO_ICON_BUTTON_OKAY,	   LOCALE_OPKG_BUTTON_INSTALL },
-	{ NEUTRINO_ICON_BUTTON_BLUE, LOCALE_OPKG_BUTTON_UNINSTALL }
+	{ NEUTRINO_ICON_BUTTON_RED, LOCALE_OPKG_BUTTON_UNINSTALL }
 };
 
 void COPKGManager::updateMenu()
@@ -233,10 +232,14 @@ int COPKGManager::showMenu()
 	getPkgData(OM_LIST);
 	getPkgData(OM_LIST_UPGRADEABLE);
 
-	menu = new CMenuWidget(g_Locale->getText(LOCALE_SERVICEMENU_UPDATE), NEUTRINO_ICON_UPDATE, width, MN_WIDGET_ID_SOFTWAREUPDATE);
-	menu->addIntroItems(LOCALE_OPKG_TITLE);
+	menu = new CMenuWidget(g_Locale->getText(LOCALE_OPKG_TITLE), NEUTRINO_ICON_UPDATE, width, MN_WIDGET_ID_SOFTWAREUPDATE);
+	CMenuForwarder menuBack(LOCALE_MENU_BACK, true, NULL, NULL, NULL, CRCInput::RC_nokey, NEUTRINO_ICON_BUTTON_LEFT, NULL, true);
+	menuBack.setItemButton(!g_settings.menu_left_exit ? NEUTRINO_ICON_BUTTON_HOME : NEUTRINO_ICON_BUTTON_LEFT); 
+	menu->addItem(&menuBack);
+	menuBack.setHint(NEUTRINO_ICON_HINT_BACK, LOCALE_MENU_HINT_BACK_BRIEF);
+	menu->addItem(GenericMenuSeparatorLine);
 
-	upgrade_forwarder = new CMenuForwarder(LOCALE_OPKG_UPGRADE, true, NULL , this, pkg_types[OM_UPGRADE].c_str(), CRCInput::RC_red);
+	upgrade_forwarder = new CMenuForwarder(LOCALE_OPKG_UPGRADE, true, NULL , this, pkg_types[OM_UPGRADE].c_str(), CRCInput::RC_green);
 	upgrade_forwarder->setHint(NEUTRINO_ICON_HINT_SW_UPDATE, LOCALE_MENU_HINT_OPKG_UPGRADE);
 	menu->addItem(upgrade_forwarder);
 	menu->addItem(GenericMenuSeparatorLine);
@@ -244,9 +247,10 @@ int COPKGManager::showMenu()
 	menu_offset = menu->getItemsCount();
 
 	menu->addKey(CRCInput::RC_info, this, "rc_info");
-	menu->addKey(CRCInput::RC_blue, this, "rc_blue");
-	menu->addKey(CRCInput::RC_red, this, "rc_red");
-
+	menu->addKey(CRCInput::RC_spkr, this, "rc_spkr");
+	menu->addKey(CRCInput::RC_setup, this, "rc_setup");
+	menu->addKey(CRCInput::RC_red, this, "rc_spkr");
+	menu->addKey(CRCInput::RC_blue, this, "rc_setup");
 	pkg_vec.clear();
 	for (std::map<string, struct pkg>::iterator it = pkg_map.begin(); it != pkg_map.end(); it++) {
 		it->second.forwarder = new CMenuForwarder(it->second.desc, true, NULL , this, it->second.name.c_str());
@@ -269,26 +273,28 @@ int COPKGManager::showMenu()
 
 bool COPKGManager::hasOpkgSupport()
 {
-	string deps[] = {"/etc/opkg/opkg.conf", "/var/lib/opkg", ""};
-
-	if (find_executable(OPKG_CL).empty()) {
-		dprintf(DEBUG_INFO, "[neutrino opkg] did not find " OPKG_CL " executable\n");
+	const char *deps[] = {"opkg-cl", "opkg-key", "/etc/opkg/opkg.conf", "/var/lib/opkg", NULL};
+	for (const char **d = deps; *d; d++) {
+		if (*d[0] != '/') {
+			bool found = false;
+			const char *path[] = {"/bin", "/usr/bin", "/sbin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin", NULL};
+			for (const char **p = path; *p && !found; p++)
+				found = access(std::string(*p) + "/" + *d, R_OK);
+			if (!found)
+				return false;
+			continue;
+		} else if(!access(*d, R_OK))
+			continue;
+		printf("[neutrino opkg] %s not found\n", *d);
 		return false;
 	}
-	for (size_t i = 0; !deps[i].empty(); i++) {
-		dprintf(DEBUG_INFO,  "[neutrino opkg] check if %s is available...\n", deps[i].c_str());
-		if(access(deps[i].c_str(), R_OK) !=0) {
-			dprintf(DEBUG_INFO,  "[neutrino opkg] %s not found\n", deps[i].c_str());
-			return false;
-		}
-	}
-
+	
 	return true;
 }
 
 void COPKGManager::getPkgData(const int pkg_content_id)
 {
-	dprintf(DEBUG_INFO, "COPKGManager: executing %s\n", pkg_types[pkg_content_id].c_str());
+	printf("COPKGManager: executing %s\n", pkg_types[pkg_content_id].c_str());
 
 	switch (pkg_content_id) {
 		case OM_LIST:
@@ -372,7 +378,7 @@ fprintf(stderr, "execCmd(%s)\n", cmdstr);
 		return res;
 	} else {
 		cmd += " 2>/dev/null >&2";
-		int r = my_system(3, "/bin/sh", "-c", cmd.c_str());
+		int r = system(cmd.c_str());
 		if (r == -1)
 			return r;
 		return WEXITSTATUS(r);
